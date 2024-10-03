29000a6610d975b6238634a5a92fcbf3dc8cdcb0
nathanmedeiros <130366446+nathanmedeiros700444@users.noreply.github.com>
2024-10-03 11:41:35 +0000

    index atualizado

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const app = express();
 const PORT = 3350;
 
 app.get("/", function (req, res) {
-  //res.sendFile("Ola!...");
-  console.log(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+  //console.log(__dirname + "/src/views/index.html");
 });
+
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+  //console.log(__dirname + "/src/views/about.html");
+});
+
+app.get("/404", function (req, res) {
+  res.sendFile(__dirname + "/src/views/404.html");
+  //console.log(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
